3110980d6f15101dd65ae410ec32721dbecc9681
brandonHafley <bchafley@gmail.com>
2021-04-23 14:10:00 -0500

    push existing project into repo

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const { Client, Collection } = require('discord.js');
+const { prefix, token } = require('./config.json');
+const urban = require('./commands/fun/urban.js');
+
+const client = new Client();
+client.commands = new Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+
+client.once('ready', () => {
+    console.log(`Ready! Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content} - ${message.createdAt}`);
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (command.permissions) {
+        const authorPerms = message.channel.permissionsFor(message.author);
+        if (!authorPerms || !authorPerms.has(command.permissions)) {
+            return message.reply('You can not do this!');
+        }
+    }
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
+});
+
+client.login(token);
